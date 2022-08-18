e10f7464125cdbdcd0c78b6a4258304d3f06b7f1
timurich80 <timurich80@gmail.com>
2022-08-18 17:53:29 +0300

    добавил изменение в проект

---
+++ exsample005_ConditionIfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите имя пользователя : ");
+
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ринат")
+{ 
+    Console.WriteLine("Приветствую тебя мой хозяин Ринат");
+}
+
+else 
+{
+    Console.Write("Здравствуй, ");
+    Console.WriteLine(username);
+}
